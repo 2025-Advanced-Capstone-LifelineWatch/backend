19e080fbe15caa65eaa8ac69e98449cce5e359df
gituserchan <pcj4142@naver.com>
2025-03-23 14:22:48 +0900

    Initial settings

---
+++ src/main/java/com/kgu/life_watch/LifeWatchApplication.java
@@ -0,0 +1,13 @@
+package com.kgu.life_watch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeWatchApplication.class, args);
+	}
+
+}
