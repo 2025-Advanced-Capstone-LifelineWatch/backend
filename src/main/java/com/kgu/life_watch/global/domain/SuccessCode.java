d4fa7da6b7e34cb6704ff2a5d71dbc874b1fd832
gyuseon <mn5592@naver.com>
2025-04-01 21:14:45 +0900

    Feat : 커스텀 예외를 구현하기 위한 클래스들 생성 (#1)

---
+++ src/main/java/com/kgu/life_watch/global/domain/SuccessCode.java
@@ -0,0 +1,27 @@
+package com.kgu.life_watch.global.domain;
+
+import com.kgu.life_watch.global.exception.ErrorCode;
+import com.kgu.life_watch.global.exception.LifelineException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum SuccessCode {
+
+    REQUEST_OK(HttpStatus.OK, "올바른 요청입니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    // 메시지를 기반으로 ErrorCode를 찾는 정적 메서드
+    public static SuccessCode fromMessage(String message) {
+        for (SuccessCode successCode : SuccessCode.values()) {
+            if (successCode.getMessage().equals(message)) {
+                return successCode;
+            }
+        }
+        throw LifelineException.from(ErrorCode.ENUM_CONVERSION_ERROR);
+    }
+}
