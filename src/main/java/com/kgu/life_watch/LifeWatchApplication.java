54a58a26ba84e98f9922c3ce0057976763f74535
gyuseon <mn5592@naver.com>
2025-04-01 21:07:17 +0900

    Feat : BaseEntity 생성 (#1)

--- src/main/java/com/kgu/life_watch/LifeWatchApplication.java
+++ src/main/java/com/kgu/life_watch/LifeWatchApplication.java
@@ -2,8 +2,10 @@ package com.kgu.life_watch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LifeWatchApplication {
 
 	public static void main(String[] args) {
