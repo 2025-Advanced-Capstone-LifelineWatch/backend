7d78a2a87a1074bb0b1ea635266bf25d6c9471f1
gituserchan <pcj4142@naver.com>
2025-04-06 13:29:41 +0900

    Feat: 로그인 기능 구현 #5

--- src/main/java/com/kgu/life_watch/domain/user/entity/User.java
+++ src/main/java/com/kgu/life_watch/domain/user/entity/User.java
@@ -3,6 +3,7 @@ package com.kgu.life_watch.domain.user.entity;
 import com.kgu.life_watch.global.domain.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -20,13 +21,13 @@ public class User extends BaseEntity {
     private String name;
 
     @Column(nullable = false, unique = true)
-    private String loginId;  // <- 로그인용 ID
+    private String loginId;
 
     @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
-    private String password; // 암호화 저장됨 (BCrypt)
+    private String password;
 
     @Column(nullable = false)
     private String phoneNumber;
@@ -37,58 +38,35 @@ public class User extends BaseEntity {
     @Column(nullable = false, name = "resident_registration_number")
     private String rrn;
 
-    @Column(nullable = false, name = "disability_registration_number")
-    private String drn;
-
-    @Column(nullable = false, name = "social_worker_id")
-    private Long socialWorkerId;
-
     @Column(nullable = false)
-    private String birthDate;
+    private LocalDate birthDate;
 
     @Column(nullable = false)
     private String gender;
 
-    @Column
-    private String protectorContact; // 독거노인 전용
-
-    @Column(name = "assigned_elder_id")
-    private Long assignedElderId; // 사회복지사 전용 (optional)
-
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private ElderlyProfile elderlyProfile;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private SocialWorkerProfile socialWorkerProfile;
 
     public enum Role {
         USER,
         ADMIN,
         SOCIAL_WORKER
     }
-    /**
-     *
-     * 필수 :
-     * 이름,
-     * 아이디,
-     * 패스워드,
-     * 생년월일(YYYY/MM/DD),
-     * 성별,
-     * 주소,
-     * 주민번호,
-     * 담당사회복지사 아이디,
-     * 핸드폰 번호
-     * 보호자 연락처
-     *
-     *
-     * 필수 :
-     * 이름,
-     * 아이디,
-     * 패스워드,
-     * 생년월일(YYYY/MM/DD),
-     * 성별,
-     * 주소,
-     * 주민번호,
-     * 핸드폰 번호
-     *
-     * 선택 :
-     * 담당환자 아이디
-     */
+    public void setElderlyProfile(ElderlyProfile elderlyProfile) {
+        this.elderlyProfile = elderlyProfile;
+        elderlyProfile.setUser(this);
+    }
+
+    public void setSocialWorkerProfile(SocialWorkerProfile socialWorkerProfile) {
+        this.socialWorkerProfile = socialWorkerProfile;
+        socialWorkerProfile.setUser(this);
+    }
+
 }
