dd1c440fbe3046e6f22c672b2d27e2bcbf656c7d
gyuseon <mn5592@naver.com>
2025-04-11 21:37:17 +0900

    Refactor : User id 필드명 변경(#8)

--- src/main/java/com/kgu/life_watch/domain/user/entity/User.java
+++ src/main/java/com/kgu/life_watch/domain/user/entity/User.java
@@ -12,12 +12,12 @@ import com.kgu.life_watch.global.domain.BaseEntity;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-@Table(name = "`user`")
+@Table(name = "users")
 public class User extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long userId;
+  private Long id;
 
   @Column(nullable = false)
   private String name;
