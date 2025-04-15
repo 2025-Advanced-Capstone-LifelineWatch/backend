ffde576241d577cd06ee915d7754672acf9bcc9a
gyuseon <mn5592@naver.com>
2025-04-15 23:50:19 +0900

    Feat: WebSocker 설정 추가 (#8)

--- src/main/java/com/kgu/life_watch/global/config/WebSocketConfig.java
+++ src/main/java/com/kgu/life_watch/global/config/WebSocketConfig.java
@@ -1,23 +1,27 @@
 package com.kgu.life_watch.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import lombok.RequiredArgsConstructor;
+
+import com.kgu.life_watch.global.handler.StompHandler;
+
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  private final StompHandler stompHandler;
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-    // socketJs 클라이언트가 WebSocket 핸드셰이크를 하기 위해 연결할 endpoint를 지정할 수 있다.
-    registry
-        .addEndpoint("/chat/inbox")
-        .setAllowedOriginPatterns("*") // cors 허용을 위해 꼭 설정해주어야 한다.
-        .withSockJS(); // 웹소켓을 지원하지 않는 브라우저는 sockJS를 사용하도록 한다.
+    registry.addEndpoint("/chat").setAllowedOriginPatterns("*"); // cors 허용을 위해 꼭 설정해주어야
   }
 
   @Override
@@ -29,5 +33,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     // 클라이언트로부터 메시지를 받을 api의 prefix를 설정한다.
     // publish
     registry.setApplicationDestinationPrefixes("/pub");
+  }
+
+  @Override
+  public void configureClientInboundChannel(ChannelRegistration registration) {
+    registration.interceptors(stompHandler);
   }
 }
