9a06386100d96af12be56df96b2e1324acde438c
gyuseon <mn5592@naver.com>
2025-04-21 06:41:36 +0900

    Chore: RabbitMQ 관련 설정 추가 (#15)

--- src/main/java/com/kgu/life_watch/global/config/WebSocketConfig.java
+++ src/main/java/com/kgu/life_watch/global/config/WebSocketConfig.java
@@ -1,8 +1,10 @@
 package com.kgu.life_watch.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -18,6 +20,25 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   private final StompHandler stompHandler;
 
+  // RabbitMQ 연결에 필요한 값들을 @Value로 설정
+  @Value("${rabbitmq.host}")
+  private String host;
+
+  @Value("${rabbitmq.relay.port}")
+  private int port;
+
+  @Value("${rabbitmq.relay.system-login}")
+  private String systemLogin;
+
+  @Value("${rabbitmq.relay.client-passcode}")
+  private String systemPasscode;
+
+  @Value("${rabbitmq.relay.client-login}")
+  private String clientLogin;
+
+  @Value("${rabbitmq.relay.client-passcode}")
+  private String clientPasscode;
+
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
 
@@ -26,9 +47,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
-    // 메모리 기반 메시지 브로커가 해당 api를 구독하고 있는 클라이언트에게 메시지를 전달한다.
-    // to subscriber
-    registry.enableSimpleBroker("/sub");
+
+    // RabbitMQ와 STOMP Broker Relay 설정
+    registry.setPathMatcher(new AntPathMatcher("."));
+
+    // RabbitMQ와 STOMP Broker Relay 설정
+    registry
+        .enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue")
+        .setRelayHost(host) // RabbitMQ 호스트
+        .setRelayPort(port) // RabbitMQ 포트
+        .setSystemLogin(systemLogin) // 시스템 로그인
+        .setSystemPasscode(systemPasscode) // 시스템 비밀번호
+        .setClientLogin(clientLogin) // 클라이언트 로그인
+        .setClientPasscode(clientPasscode); // 클라이언트 비밀번호
 
     // 클라이언트로부터 메시지를 받을 api의 prefix를 설정한다.
     // publish
