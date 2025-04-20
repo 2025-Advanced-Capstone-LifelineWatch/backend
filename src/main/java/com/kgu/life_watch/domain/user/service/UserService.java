29d18dcfb4869f8218b8536e8920430e2c356aec
gyuseon <mn5592@naver.com>
2025-04-21 07:59:32 +0900

    Feat: 프로필 조회 기능 구현 (#8)

--- src/main/java/com/kgu/life_watch/domain/user/service/UserService.java
+++ src/main/java/com/kgu/life_watch/domain/user/service/UserService.java
@@ -5,8 +5,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
+import com.kgu.life_watch.domain.user.dto.UserProfileResponse;
 import com.kgu.life_watch.domain.user.entity.ElderlyProfile;
 import com.kgu.life_watch.domain.user.entity.SocialWorkerProfile;
+import com.kgu.life_watch.domain.user.entity.User;
 import com.kgu.life_watch.domain.user.repository.ElderlyProfileRepository;
 import com.kgu.life_watch.domain.user.repository.SocialWorkerProfileRepository;
 import com.kgu.life_watch.global.exception.ErrorCode;
@@ -47,5 +49,10 @@ public class UserService {
     socialWorker.getAssignedSeniors().remove(elderly);
     // 노인의 사회복지사 연관관계 해제
     elderly.setSocialWorkerProfile(null);
+  }
+
+  @Transactional(readOnly = true)
+  public UserProfileResponse getProfile(User user) {
+    return UserProfileResponse.toDto(user);
   }
 }
