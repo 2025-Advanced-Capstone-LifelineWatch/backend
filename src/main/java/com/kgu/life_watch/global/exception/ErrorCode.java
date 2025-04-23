07fe7affcabcaa21af45f57359eecab1b38f36e9
gituserchan <pcj4142@naver.com>
2025-04-23 17:00:31 +0900

    Feat: SMS 인증번호 검증 틀렸을 시 에러 코드 추가 (#19)

--- src/main/java/com/kgu/life_watch/global/exception/ErrorCode.java
+++ src/main/java/com/kgu/life_watch/global/exception/ErrorCode.java
@@ -64,7 +64,10 @@ public enum ErrorCode {
   PROFILE_IMAGE_REQUIRED(HttpStatus.BAD_REQUEST, "프로필 이미지 선택은 필수입니다."),
 
   // alarm
-  ALARM_NOT_FOUND(HttpStatus.NOT_FOUND, "토큰을 찾을 수 없습니다.");
+  ALARM_NOT_FOUND(HttpStatus.NOT_FOUND, "토큰을 찾을 수 없습니다."),
+
+  // sns
+  SMS_VERIFICATION_FAILED(HttpStatus.UNAUTHORIZED, "검증 코드가 일치하지 않습니다!");
 
   private final HttpStatus status;
   private final String message;
