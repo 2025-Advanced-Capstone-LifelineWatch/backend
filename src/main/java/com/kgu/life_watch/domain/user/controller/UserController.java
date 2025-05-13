355c4de09ad84c8d3f2ac97de9a0047c16b43fb7
gyuseon <mn5592@naver.com>
2025-05-13 13:03:41 +0900

    Feat: 사회복지사가 노인 할당시 채팅방 생성 코드 추가 (#28)

--- src/main/java/com/kgu/life_watch/domain/user/controller/UserController.java
+++ src/main/java/com/kgu/life_watch/domain/user/controller/UserController.java
@@ -1,9 +1,9 @@
 package com.kgu.life_watch.domain.user.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +12,7 @@ import com.kgu.life_watch.domain.user.dto.ElderlyAssignmentRequest;
 import com.kgu.life_watch.domain.user.dto.UserProfileResponse;
 import com.kgu.life_watch.domain.user.entity.User;
 import com.kgu.life_watch.domain.user.service.UserService;
+import com.kgu.life_watch.global.domain.SuccessCode;
 import com.kgu.life_watch.global.dto.response.ApiResponse;
 import com.kgu.life_watch.global.security.CustomUserDetails;
 
@@ -26,6 +27,7 @@ public class UserController {
   // 현재 인증된 사용자의 정보를 주입받는다.
   // SecurityContextHolder에 저장된 Authentication에서 CustomUserDetails를 꺼내어 자동 주입함
   @GetMapping("/me")
+  @Operation(summary = "유저 정보 조회 API", description = "유저 정보를 조회하는 API입니다.")
   public ApiResponse<UserProfileResponse> getCurrentUser(
       @AuthenticationPrincipal CustomUserDetails userDetails) {
     User user = userDetails.user(); // CustomUserDetails 내부에 저장된 실제 User 엔티티를 꺼냄
@@ -33,6 +35,7 @@ public class UserController {
   }
 
   @GetMapping("/role-check")
+  @Operation(summary = "유저 역할 조회 API", description = "유저 역할을 조회하는 API입니다.")
   public ApiResponse<String> checkRole(@AuthenticationPrincipal CustomUserDetails userDetails) {
     User user = userDetails.user();
 
@@ -46,16 +49,17 @@ public class UserController {
 
   // 노인 할당
   @PostMapping("/assign-elderly")
-  public ResponseEntity<Void> assignElderly(@RequestBody @Valid ElderlyAssignmentRequest request) {
+  @Operation(summary = "노인 할당 API", description = "담당 노인을 할당하는 API입니다.")
+  public ApiResponse<Void> assignElderly(@RequestBody @Valid ElderlyAssignmentRequest request) {
     userService.assignElderly(request.elderlyId(), request.socialWorkerId());
-    return ResponseEntity.ok().build();
+    return new ApiResponse<>(SuccessCode.REQUEST_OK);
   }
 
   // 노인 할당해제
   @PostMapping("/unassign-elderly")
-  public ResponseEntity<Void> unassignElderly(
-      @RequestBody @Valid ElderlyAssignmentRequest request) {
+  @Operation(summary = "노인 할당 해제 API", description = "담당 노인 할당을 해제 API입니다.")
+  public ApiResponse<Void> unassignElderly(@RequestBody @Valid ElderlyAssignmentRequest request) {
     userService.unassignElderly(request.elderlyId(), request.socialWorkerId());
-    return ResponseEntity.ok().build();
+    return new ApiResponse<>(SuccessCode.REQUEST_OK);
   }
 }
