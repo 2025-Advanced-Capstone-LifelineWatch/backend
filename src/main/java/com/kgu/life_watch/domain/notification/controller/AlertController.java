eeda8e7a04eecdb2e56031f3910af947ab9c5d23
gituserchan <pcj4142@naver.com>
2025-05-14 16:19:05 +0900

    Refactor: 알람 api 리팩토링(#17)

--- src/main/java/com/kgu/life_watch/domain/notification/controller/AlertController.java
+++ src/main/java/com/kgu/life_watch/domain/notification/controller/AlertController.java
@@ -1,6 +1,5 @@
 package com.kgu.life_watch.domain.notification.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 
 import com.kgu.life_watch.domain.notification.dto.EmergencyAlertRequest;
 import com.kgu.life_watch.domain.notification.service.FirebaseMessageService;
+import com.kgu.life_watch.global.domain.SuccessCode;
+import com.kgu.life_watch.global.dto.response.ApiResponse;
 
 @RestController
 @RequestMapping("/api/alert")
@@ -22,9 +23,9 @@ public class AlertController {
   @Operation(
       summary = "노인의 이상건강에 관한 경고 알람 API",
       description = "노인의 이상건강에 관한 경고 알람을 담당 사회복지사에게 전달하는 API입니다.")
-  public ResponseEntity<Void> handleEmergency(@RequestBody EmergencyAlertRequest request) {
+  public ApiResponse<Void> handleEmergency(@RequestBody EmergencyAlertRequest request) {
     firebaseMessageService.sendEmergencyAlert(
         request.elderlyId(), request.predictionLabel(), request.explanation());
-    return ResponseEntity.ok().build();
+    return new ApiResponse<>(SuccessCode.REQUEST_OK);
   }
 }
