e2af1aa6cfd22f59ddb82dc62eba05f4629e29d3
gyuseon <mn5592@naver.com>
2025-05-19 13:32:54 +0900

    Refactor: CORS 설정 변경 (#19)

--- src/main/java/com/kgu/life_watch/global/config/SecurityConfig.java
+++ src/main/java/com/kgu/life_watch/global/config/SecurityConfig.java
@@ -82,7 +82,8 @@ public class SecurityConfig {
         Arrays.asList(
             "http://localhost:3000",
             "http://localhost:8080",
-            "https://jiangxy.github.io")); // 추후에 배포시 수정 필요
+            "https://jiangxy.github.io",
+            "https://server.lifewatch.store")); // 추후에 배포시 수정 필요
     configuration.setAllowedMethods(
         Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD")); // 허용할 HTTP 메서드
     configuration.setAllowedHeaders(
