1f2273843930278b5c0082bf5c2e45e9a207dc25
gituserchan <pcj4142@naver.com>
2025-05-26 17:41:37 +0900

    Refactor : 오류나는 코드 고치기  (#17)

--- src/main/java/com/kgu/life_watch/domain/notification/repository/MedicineAlarmRepository.java
+++ src/main/java/com/kgu/life_watch/domain/notification/repository/MedicineAlarmRepository.java
@@ -3,6 +3,7 @@ package com.kgu.life_watch.domain.notification.repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,20 +13,17 @@ import com.kgu.life_watch.domain.notification.entity.MedicineAlarm;
 public interface MedicineAlarmRepository extends JpaRepository<MedicineAlarm, Long> {
 
   @Query("SELECT m FROM MedicineAlarm m WHERE m.user.id = :userId")
-  List<MedicineAlarm> findAllByUserId(Long userId);
+  List<MedicineAlarm> findAllByUserId(@Param("userId") Long userId);
 
   List<MedicineAlarm> findAllByAlarmGroupId(Long alarmGroupId);
 
+  @EntityGraph(attributePaths = {"user.elderlyProfile.socialWorkerProfile.user"})
   @Query(
       """
-SELECT a
-FROM MedicineAlarm a
-JOIN FETCH a.user u
-JOIN FETCH ElderlyProfile e ON e.user = u
-JOIN FETCH e.socialWorkerProfile sw
-JOIN FETCH sw.user
-WHERE a.time >= :start AND a.time < :end AND a.status = 'SCHEDULED'
-""")
+    SELECT a FROM MedicineAlarm a
+    WHERE a.time >= :start AND a.time < :end
+    AND a.status = 'SCHEDULED'
+  """)
   List<MedicineAlarm> findAlarmsWithAllUserInfoByTimeBetween(
       @Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 }
