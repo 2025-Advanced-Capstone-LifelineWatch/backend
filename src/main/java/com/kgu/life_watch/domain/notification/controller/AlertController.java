dda4ec562ba1f8131673f463cd315bf30a893a0c
gituserchan <pcj4142@naver.com>
2025-05-30 11:26:17 +0900

    Feat : 유효값 처리랑 + 비정상일때 로그 저장되게  (#17)

--- src/main/java/com/kgu/life_watch/domain/notification/controller/AlertController.java
+++ src/main/java/com/kgu/life_watch/domain/notification/controller/AlertController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import com.kgu.life_watch.domain.notification.dto.EmergencyAlertRequest;
@@ -28,7 +29,7 @@ public class AlertController {
   @Operation(
       summary = "노인의 이상건강에 관한 경고 알람 API",
       description = "노인의 이상건강에 관한 경고 알람을 담당 사회복지사에게 전달하는 API입니다.")
-  public ApiResponse<Void> handleEmergency(@RequestBody EmergencyAlertRequest request) {
+  public ApiResponse<Void> handleEmergency(@Valid @RequestBody EmergencyAlertRequest request) {
     firebaseMessageService.sendEmergencyAlert(
         request.elderlyId(), request.predictionLabel(), request.explanation());
     return new ApiResponse<>(SuccessCode.REQUEST_OK);
